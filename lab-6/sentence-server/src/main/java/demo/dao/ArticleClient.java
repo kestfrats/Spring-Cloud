c1fb2f5dce51bccf14488043b8122a4c9fe515b4
Cenk Salfur <kestfrats@gmail.com>
2018-12-29 16:25:03 +0300

    New Projects

---
+++ lab-6/sentence-server/src/main/java/demo/dao/ArticleClient.java
@@ -0,0 +1,14 @@
+package demo.dao;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import demo.domain.Word;
+
+@FeignClient(name = "ARTICLE")
+public interface ArticleClient {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public Word getWord();
+}
